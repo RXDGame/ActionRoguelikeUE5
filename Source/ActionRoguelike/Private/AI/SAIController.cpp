6666f8d775c84b9b8aa2bc4e2bdde54fa2cc1a3b
Rodrigo Dias <rdias@kokku.com.br>
2023-07-14 17:08:20 -0300

    Created AI classes & Setup first enemy Behavior Tree

---
+++ Source/ActionRoguelike/Private/AI/SAIController.cpp
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/SAIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "Kismet/GameplayStatics.h"
+
+
+// Called when the game starts or when spawned
+void ASAIController::BeginPlay()
+{
+	Super::BeginPlay();
+	RunBehaviorTree(BehaviorTree);
+
+	APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+	if(PlayerPawn)
+	{
+		GetBlackboardComponent()->SetValueAsVector("MoveToLocation", PlayerPawn->GetActorLocation());
+		GetBlackboardComponent()->SetValueAsObject("TargetActor", PlayerPawn);
+	}
+}
---
+++ Source/ActionRoguelike/Public/AI/SAIController.h
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "SAIController.generated.h"
+
+UCLASS()
+class ACTIONROGUELIKE_API ASAIController : public AAIController
+{
+	GENERATED_BODY()
+
+protected:
+
+	UPROPERTY(EditDefaultsOnly, Category = "AI")
+	TObjectPtr<UBehaviorTree> BehaviorTree;
+	
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+};
