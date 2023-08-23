82517ec09bf676cd2c795f4b9e932cd4e5c58cc0
Rodrigo Dias <rdias@kokku.com.br>
2023-08-23 08:57:08 -0300

    Created pause menu

---
+++ Source/ActionRoguelike/Private/SPlayerController.cpp
@@ -0,0 +1,35 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SPlayerController.h"
+
+#include "Blueprint/UserWidget.h"
+
+void ASPlayerController::TogglePause()
+{
+	if(PauseMenuInstance && PauseMenuInstance->IsInViewport())
+	{
+		PauseMenuInstance->RemoveFromParent();
+		PauseMenuInstance = nullptr;
+
+		bShowMouseCursor = false;
+		SetInputMode(FInputModeGameOnly());
+		return;
+	}
+	
+	PauseMenuInstance = CreateWidget<UUserWidget>(this, PauseMenuClass);
+	if(PauseMenuInstance)
+	{
+		PauseMenuInstance->AddToViewport(100);
+
+		bShowMouseCursor = true;
+		SetInputMode(FInputModeUIOnly());
+	}
+}
+
+void ASPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	InputComponent->BindAction("PauseGame", IE_Pressed, this, &ASPlayerController::TogglePause);
+}
---
+++ Source/ActionRoguelike/Public/SPlayerController.h
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "SPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACTIONROGUELIKE_API ASPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+protected:
+	UPROPERTY(EditAnywhere, Category = "UI")
+	TSubclassOf<UUserWidget> PauseMenuClass;
+
+	UPROPERTY()
+	UUserWidget* PauseMenuInstance;
+
+	UFUNCTION(BlueprintCallable)
+	void TogglePause();
+
+	void SetupInputComponent() override;
+};
