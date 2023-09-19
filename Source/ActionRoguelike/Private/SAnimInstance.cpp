32beafc7cd9bb18983f8f20ab18371530386ae18
Rodrigo Dias <rdias@kokku.com.br>
2023-09-19 11:39:10 -0300

    finished extra animations

---
+++ Source/ActionRoguelike/Private/SAnimInstance.cpp
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SAnimInstance.h"
+
+#include "GameplayTagContainer.h"
+#include "ActionSystem/SActionComponent.h"
+
+void USAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	const AActor* OwningActor = GetOwningActor();
+	if(OwningActor)
+	{
+		ActionComponent = OwningActor->GetComponentByClass<USActionComponent>();
+	}
+}
+
+void USAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	const FGameplayTag StunnedTag = FGameplayTag::RequestGameplayTag("Status.Stunned");
+	if(ActionComponent)
+	{
+		bIsStunned = ActionComponent->ActiveGameplayTags.HasTag(StunnedTag);
+	}
+}
