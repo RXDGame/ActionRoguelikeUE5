1a08ae6bed085cd4122b4391a6891c18c1b76c62
Rodrigo Dias <rodrigoaadias@hotmail.com>
2025-02-28 06:50:51 -0300

    changed to Unreal 5.5

--- Source/ActionRoguelike/Private/SPickup.cpp
+++ Source/ActionRoguelike/Private/SPickup.cpp
@@ -1,10 +1,8 @@
 ﻿// Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "SPickup.h"
 #include "Components/SphereComponent.h"
 #include "Net/UnrealNetwork.h"
-
 
 // Sets default values
 ASPickup::ASPickup()
